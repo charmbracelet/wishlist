d49b60e97e849c28290c4207c71935a48c7d54c0
Carlos A Becker <caarlos0@gmail.com>
2022-04-04 11:00:41 -0300

    test: fixes

---
+++ wishlist_test.go
@@ -0,0 +1,26 @@
+package wishlist
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEndointToListItems(t *testing.T) {
+	result := endpointsToListItems([]*Endpoint{
+		{Name: "name", Address: "anything"},
+	})
+
+	require.Len(t, result, 1)
+	item := result[0]
+	require.Equal(t, "name", item.FilterValue())
+}
+
+func TestNewWishlist(t *testing.T) {
+	t.Run("local", func(t *testing.T) {
+		lm := NewListing([]*Endpoint{{Name: "name", Address: "anything"}}, nil)
+		require.Len(t, lm.endpoints, 1)
+		require.Nil(t, lm.session)
+		require.Nil(t, lm.handoff)
+	})
+}
