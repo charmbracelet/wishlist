1c7e8b3682682a798816fab96943f031095a2f08
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-06-10 23:19:51 -0300

    fix: smarter list delegate height (#77)
    * fix: smarter list delegate height
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * refactor: improvements
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * test: tests
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * fix: interface conversion
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * fix: lint
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

---
+++ listitem.go
@@ -0,0 +1,53 @@
+package wishlist
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var noContentStyle = lipgloss.NewStyle().Faint(true).Italic(true)
+
+var _ list.Item = ItemWrapper{}
+
+// ItemWrapper wrappes an Endpoint and a set of descriptors and acts as a list.Item.
+type ItemWrapper struct {
+	endpoint    *Endpoint
+	descriptors []descriptor
+}
+
+// FilterValue to abide the list.Item interface.
+func (i ItemWrapper) FilterValue() string { return i.endpoint.Name }
+
+// Title to abide the list.Item interface.
+func (i ItemWrapper) Title() string { return i.endpoint.Name }
+
+// Description to abide the list.Item interface.
+func (i ItemWrapper) Description() string {
+	var lines []string
+	for _, desc := range i.descriptors {
+		lines = append(lines, desc(i.endpoint))
+	}
+	return strings.Join(lines, "\n")
+}
+
+type descriptor func(e *Endpoint) string
+
+func withSSHURL(i *Endpoint) string {
+	return Link{URL: "ssh://" + i.Address}.String()
+}
+
+func withLink(i *Endpoint) string {
+	if l := i.Link.String(); l != "" {
+		return l
+	}
+	return noContentStyle.Render("no link")
+}
+
+func withDescription(i *Endpoint) string {
+	if desc := strings.Split(i.Desc, "\n")[0]; desc != "" {
+		return desc
+	}
+	return noContentStyle.Render("no description")
+}
