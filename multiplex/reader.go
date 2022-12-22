bed3320cb8308280988dd2add77e1c4021e115bf
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-12-22 14:04:36 -0300

    fix: race condition in multiplex/writer (#132)
    * fix: race condition in multiplex/writer
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * chore: lint
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * chore: lint
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    * fix: lint
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- multiplex/reader.go
+++ multiplex/reader.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"log"
+	"sync"
 )
 
 // ResetableReader is an io.Reader that can be "reset", i.e.: cleared of
@@ -16,23 +17,23 @@ type ResetableReader interface {
 // Reader keeps reading r and writing to 2 other readers, which are returned.
 // It stops only when the done channel is notified.
 func Reader(r io.Reader, done <-chan bool) (ResetableReader, ResetableReader) {
-	var r1 bytes.Buffer
-	var r2 bytes.Buffer
+	var r1 syncWriter
+	var r2 syncWriter
 
 	rch := make(chan bool, 1)
+	var once sync.Once
 
 	w := io.MultiWriter(&r1, &r2)
 	go func() {
-		first := true
 		for {
 			select {
 			case <-done:
 				return
 			default:
-				if first {
-					first = false
+				once.Do(func() {
 					rch <- true
-				}
+				})
+
 				buf := [256]byte{}
 				n, err := r.Read(buf[:])
 				if err != nil {
@@ -51,4 +52,35 @@ func Reader(r io.Reader, done <-chan bool) (ResetableReader, ResetableReader) {
 	// waits for the first read to start
 	<-rch
 	return &r1, &r2
+}
+
+var (
+	_ io.ReadWriter   = &syncWriter{}
+	_ ResetableReader = &syncWriter{}
+)
+
+type syncWriter struct {
+	b  bytes.Buffer
+	mu sync.RWMutex
+}
+
+// Reset implements ResetableReader.
+func (w *syncWriter) Reset() {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.b.Reset()
+}
+
+// Read implements io.Reader.
+func (w *syncWriter) Read(p []byte) (n int, err error) {
+	w.mu.RLock()
+	defer w.mu.RUnlock()
+	return w.b.Read(p) //nolint: wrapcheck
+}
+
+// Write implements io.Writer.
+func (w *syncWriter) Write(p []byte) (n int, err error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	return w.b.Write(p) //nolint: wrapcheck
 }
--- multiplex/reader_test.go
+++ multiplex/reader_test.go
@@ -30,7 +30,28 @@ func TestMultiplex(t *testing.T) {
 		require.Equal(t, s, string(b2))
 	})
 
-	t.Run("err", func(t *testing.T) {
+	t.Run("reset", func(t *testing.T) {
+		var b bytes.Buffer
+
+		const s = "this in the other hand is a test"
+		_, err := b.WriteString(s)
+		require.NoError(t, err)
+
+		done := make(chan bool, 1)
+		t.Cleanup(func() { done <- true })
+		r1, r2 := Reader(&b, done)
+
+		b1, err := io.ReadAll(r1)
+		require.NoError(t, err)
+		require.Equal(t, s, string(b1))
+
+		r2.Reset()
+		b2, err := io.ReadAll(r2)
+		require.NoError(t, err)
+		require.Empty(t, string(b2))
+	})
+
+	t.Run("read err", func(t *testing.T) {
 		r := iotest.ErrReader(io.ErrClosedPipe)
 		done := make(chan bool, 1)
 		t.Cleanup(func() { done <- true })
