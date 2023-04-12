79e5e854b67ca3b149857689d7d9f7b3563cb52d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-04-12 14:20:07 -0300

    Update client_auth.go
    Co-authored-by: Ayman Bagabas <ayman.bagabas@gmail.com>

--- client_auth.go
+++ client_auth.go
@@ -149,7 +149,7 @@ func tryNewKey() (gossh.AuthMethod, error) {
 		return nil, fmt.Errorf("could not create client key: %w", err)
 	}
 
-	key, err := keygen.New(path, keygen.WithKeyType(keygen.Ed25519))
+	key, err := keygen.New(path+"_ed25519", keygen.WithKeyType(keygen.Ed25519))
 	if err != nil {
 		return nil, fmt.Errorf("could not create new client key at %q: %w", path, err)
 	}
